59aee06d17287fda1206f787fa7a76be789221ba
Tilahun Assefa Demissie <til.assf98@gmail.com>
2024-05-10 10:46:51 -0500

    import browseranimationmodule to fix header menu error

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideAnimations } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule, provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
 import { BrowserModule } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
@@ -10,7 +10,7 @@ import { ModalModule } from 'ngx-bootstrap/modal';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideAnimations(),
-    importProvidersFrom(BrowserModule, CollapseModule.forRoot(), ModalModule.forRoot()),DatePipe,
+    importProvidersFrom(BrowserModule, BrowserAnimationsModule, CollapseModule.forRoot(), ModalModule.forRoot()),DatePipe,
     provideRouter(routes),
     provideHttpClient(),
   ]
